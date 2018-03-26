3a293a505681103ec09561640de03f79e0487095
Ian Haywood <ian@haywood.id.au>
2018-03-26 13:43:31 +1100

    set executable

--- backend/aud_gateway/withdrawals.py
+++ backend/aud_gateway/withdrawals.py
@@ -1,20 +1,32 @@
+#!/usr/bin/python3
 
 from bitshares.account import Account
-import psycopg2
+from bitshares.memo import Memo
+import psycopg2, psycopg2.extras
 from bitshares import BitShares
-import os, sys
+from bitshares.account import Account
+from bitshares.asset import Asset
+import os, sys, re, traceback, os.path, os
 from decimal import Decimal
+from configparser import ConfigParser
+
+import engine
+
+eng = engine.get_engine(config)
+
+config = ConfigParser()
+config.read(['/etc/gateway.conf', os.path.expanduser('~/.config/gateway.conf')])
 
 bitshares = BitShares()
-bitshares.wallet.unlock(os.environ['UNLOCK'])
+bitshares.wallet.unlock(config['gateway']['passphrase'])
+memoObj = Memo()
+memoObj.unlock_wallet(config['gateway']['passphrase'])
 
-ACCOUNT='river.aud4'
-ACCOUNT_ID='1.2.650660'
-ASSET='1.3.117' # bitAUD
 
-acct = Account(ACCOUNT)
+acct = Account(config['gateway']['account'])
 
-conn = psycopg2.connect(host="",database="river", user="pi")
+conn = psycopg2.connect(host=config['db'].get('host',''),database=config['db']['name'], user=config['db']["user"],
+                        cursor_factory=psycopg2.extras.NamedTupleCursor)
 cur = conn.cursor()
 
 class Transaction:
@@ -25,6 +37,7 @@ class Transaction:
         self.from_ = op['from']
         self.amount = Decimal(op['amount']['amount'])/Decimal("10000")
         self.memo = op.get('memo')
+        self.asset_id = op['amount']['asset_id']
 
     def unmatched(self):
         cur.execute("select 1 from tx where bts_txid = %s",self.id_)
@@ -33,15 +46,20 @@ class Transaction:
     def get_sender(self):
         cur.execute("select * from users where account_id = %s", self.from_)
         return cur.fetchone()
-    
+
+    def get_memo(self):
+        if self.memo:
+            return memoObj.decrypt(self.memo)
+        else:
+            return ""
+        
 def fetch_tx(num=100):
     tx = list(acct.history(limit=num, only_ops=[0]))
     all_history = len(txt) < num
     # only transactions where we received AUD
     tx = [Transaction(i)
           for i in tx
-          if i['op'][1]['to'] == ACCOUNT_ID and
-          i['op'][1]['amount']['asset_id'] == ASSET]
+          if i['op'][1]['to'] == config['gateway']['account_id']]
     unmatched = [i for i in tx if i.unmatched()] 
     if not all_history and len(unmatched) == len(tx):
         # every history item we got was unmatched: so we need to get more history
@@ -49,23 +67,44 @@ def fetch_tx(num=100):
     else:
         return unmatched
 
+def print_run(run_id):
+    cur.execute("select * from tx where run_id = %s", run_id)
+    eng.first_line()
+    for line in cur.fetchall():
+        eng.printout(line)
+    eng.last_line()
+        
 if len(sys.argv) == 1:
     txs_pending = fetch_tx()
     if len(txs_pending) == 0:
         sys.stderr.write("No transactions\n")
         sys.exit(0)
     cur.execute("insert into run (type) values ('P') returning (id)")
-    run_id = cur.fetchone()[0]
+    run_id = cur.fetchone().id
     for tx in txs_pending:
-        sender = tx.get_sender()
-        if sender:
-            
-        else:
-            sys.stderr.write("Sender %s (%s) sent $%s but has no KYC entry\n" % (tx.from_,Account(tx.from_)['name'],tx.amount))
+        try:
+            sender = tx.get_sender()
+            if not sender:
+                raise engine.Error("Sender {} ({}) sent {] {}, but has no KYC entry\n".format(Account(tx.from_)['name'],tx.from_,tx.amount, Asset(tx.asset_id)['symbol']))
+            if tx.asset_id != config['gateway']['asset_id']:
+                raise engine.Error("Sender {} ({}) sent {] {}, which is not an allowed currency\n".format(Account(tx.from_)['name'],tx.from_,tx.amount, Asset(tx.asset_id)['symbol']))
+            memo = tx.get_memo()
+            if sender.allow_thirdparty:
+                mode, bsb, acct_no, name, ref = eng.make_payment_info(sender, memo)
+            else:
+                # memo fed as lodgement reference without processing
+                mode, bsb, acct_no, name, _ = eng.make_payment_info(sender, "")
+                ref = memo
+            ref = memo or "RIVER AUD"
+            cur.execute("insert into tx (bts_account, amount, bts_txid, \"comment\", bsb, account_no, account_name, fk_run, mode) values (%s, %s, %s, %s, %s, %s, %s, %s, %s)", tx.from_, tx.amount, tx.id_, ref, bsb, acct_no, name, run_id, mode)
+        except BaseException as e:
+            traceback.print_exc(limit=2)
+            cur.execute("insert into tx (bts_account, amount, bts_txid, \"comment\", bsb, fk_run, mode) values (%s, %s, %s, %s, %s, %s, %s, %s, %s)", tx.from_, tx.amount, tx.id_, str(e), "999999", run_id, "E")
+    conn.commit()
     print_run(run_id)
 elif sys.argv[1] == '--runs':
-    cur.execute("select id, start, (select count(*) from tx where fk_run = run.id) where \"type\" = 'P' order by start desc limit 20")
+    cur.execute("select id, start, \"type\", (select count(*) from tx where tx.fk_run = run.id) order by start desc limit 20")
     for i in cur.fetchall():
-        print("{:05}\t\t{}\t\t{}".format(*i))
+        print("{:05}\t{}\t{}\t{}".format(*i))
 else:
     print_run(int(sys.argv[1]))
